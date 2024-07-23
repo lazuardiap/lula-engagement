0c2fc3031307bbd4ee4c7f324534dc8306449e8d
Lazuardi Azhar <lazuardiazhar99@gmail.com>
2024-07-23 18:36:49 +0700

    feat: implement 2nd flower

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <Layout>
       <div className='h-full relative'>
-        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center w-[80%] space-y-10'>
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center w-[80%] space-y-8'>
           <div className='title justify-center flex relative text-2xl font-lora font-bold'>
             <h1>THE ENGAGEMENT OF</h1>
           </div>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import { Children } from 'react';
+import '../App.css';
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <div className='text-primary bg-nude h-screen w-full max-w-[400px] max-h-screen relative overflow-hidden'>
       <div className="absolute -top-16">
@@ -11,6 +12,8 @@ export default function Layout({children}) {
           {child}
         </div>
       )}
+      <img src="/assets/flower-2.png" alt="" className='w-[30%] absolute top-1/2 transform -translate-y-full -right-[3rem]' />
+      <img src="/assets/flower-2.png" alt="" className='w-[30%] absolute top-1/2 -translate-y-full -left-[3rem] transform scale-x-[-1]' />
       <div className="absolute -bottom-16 [transform:rotatex(180deg)]">
         <img src="/assets/flower-1.png" alt="" />
       </div>
